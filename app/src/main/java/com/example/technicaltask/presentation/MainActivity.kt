f66e5aa6b8dcf2f448cf0a27d6c30090a239155f
Ruslan <87707286+ruslanMobile@users.noreply.github.com>
2021-12-19 14:59:49 +0200

    create modules

--- app/src/main/java/com/example/technicaltask/presentation/MainActivity.kt
+++ app/src/main/java/com/example/technicaltask/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.technicaltask
+package com.example.technicaltask.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.technicaltask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
